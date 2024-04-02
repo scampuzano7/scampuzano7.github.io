48c9a31afd5155aeaa36f496dd8acbb33fcd0a42
Sandra <sandraicampuzano7@gmail.com>
2024-04-01 20:11:16 -0700

    Updated Background Image

--- cursortext.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const textElement = document.querySelector('.text');
-    const cursorElement = document.querySelector('.cursor');
-  
-    const textWidth = textElement.offsetWidth;
-  
-    cursorElement.style.left = textWidth + 'px';
-  });
-  
